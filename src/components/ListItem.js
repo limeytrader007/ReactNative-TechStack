c340cda367e9bc3197494e35ca5a708263678a91
Daniel Chapman <dchapman@connectwise.com>
2017-07-19 08:31:19 -0400

    Add Animations

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react';
-import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import {
+  LayoutAnimation,
+  UIManager,
+  Text,
+  TouchableWithoutFeedback,
+  View
+} from 'react-native';
 import { connect } from 'react-redux';
 import { CardSection } from './common';
 import * as actions from '../actions';
 
 class ListItem extends Component {
 
-  renderDescription(){
-    const { library, selectedLibraryId } = this.props;
+  componentWillUpdate(){
+    //Android
+    UIManager.setLayoutAnimationEnabledExperimental(true);
+    LayoutAnimation.spring();
+  }
 
-    if(library.id == selectedLibraryId){
+  renderDescription(){
+    const { library, expanded } = this.props;
+
+    if(expanded){
       return(
-        <Text>{ library.description }</Text>
+        <CardSection>
+          <Text style={{ flex: 1 }}>
+            { library.description }
+          </Text>
+        </CardSection>
       );
     }
   }
@@ -42,8 +58,9 @@ const styles = {
   }
 };
 
-const mapStateToProps = state => {
-  return { selectedLibraryId: state.selectedLibraryId };
+const mapStateToProps = (state, ownProps) => {
+  const expanded = state.selectedLibraryId == ownProps.library.id;
+  return { expanded };
 };
 
 export default connect(mapStateToProps, actions)(ListItem);
